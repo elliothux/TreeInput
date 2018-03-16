be25c40b0272f454394a8b06ba4fba9c89647c5c
胡青杨 <i@do.codes>
2018-03-16 12:19:58 +0800

    Init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Input from './component/Input';
-import { noop } from './utils';
+import CollapsedIcon from './media/collapsed.svg';
+import { noop, typesMap } from './utils';
 
 import './index.scss';
 
@@ -26,21 +27,63 @@ class TreeInput extends Component {
     }
 
     state = {
-        value: []
+        value: [],
+        collapsed: false
     };
 
-    get formatedValue() {
-        return this.state.value;
-    }
+    format = (value) => {
+        console.log(value);
+        value = value.map(i => {
+            const { name, type, label, value } = i;
+            if (type === 'message') {
+                return `${name}:${this.formatMessage(value)}`;
+            } else if (label === 'REPEATED') {
+                return `${name}:${this.formatRepeated(value)}`;
+            } else {
+                if ([undefined, null, ''].includes(value)) return false;
+                if ([typesMap.BYTES, typesMap.STRING].includes(type)) {
+                    return `${name}:"${value}"`;
+                } else {
+                    return `${name}:${value}`;
+                }
+            }
+        }).filter(i => !!i).join(',');
+        return `{${value}}`;
+    };
+    formatMessage = (value) => {
 
+    };
+    formatRepeated = (value) => {
+
+    };
+
+
+    handleToggleCollapsed = (collapsed) => {
+        this.setState({
+            collapsed: typeof collapsed === 'boolean' ?
+                collapsed : !this.state.collapsed
+        });
+    };
+
+    setValue = (node) => {
+        const { value } = node;
+        const { value: V } = this.state;
+        V.find(i => i === node).value = value;
+        const newValue = [...V];
+        this.setState({ value: newValue });
+        return newValue;
+    };
     renderInput = (node) => {
-        // console.log(node);
         const { name, type, value } = node;
         const onChange = (e, value) => {
             this.setState((prevState) => {
                 const { value: V } = prevState;
                 V.find(i => i === node).value = value;
-                return { ...V }
+                const newValue = [...V];
+                const formated = this.format(newValue);
+                console.log(formated);
+                this.props.onChange(formated);
+                return { value: newValue };
             });
         };
         return (
@@ -68,18 +111,27 @@ class TreeInput extends Component {
         }
     };
     render() {
-        const { value, value: { length } } = this.state;
+        const { value, value: { length }, collapsed } = this.state;
         const { name } = this.props;
         return (
-            <div className="tree-input">
-                <div className="tree-input-start">
-                    <span className="tree-input-expand">▼</span>
-                    <span className="tree-input-name">"{name}": </span>
+            <div className={`tree-input${collapsed ? ' tree-input-collapsed' : ''}`}>
+                <div
+                    className="tree-input-start"
+                    onClick={this.handleToggleCollapsed}
+                >
+                    <img className="tree-input-expand" src={CollapsedIcon} />
+                    <span className="tree-input-name">{name}: </span>
                     <span>{"\u007b"}</span>
+                    <span if={collapsed}> ... }</span>
                     <span className="tree-input-count">{length} Items</span>
                 </div>
                 <div className="tree-input-items">{value.map(this.renderNode)}</div>
-                <div className="tree-input-end"><span>{"\u007d"}</span></div>
+                <div
+                    className="tree-input-end"
+                    onClick={this.handleToggleCollapsed}
+                >
+                    <span>{"\u007d"}</span>
+                </div>
             </div>
         );
     }
