c8953bbe408fc5f28c4d0191be6b69f82a89a462
胡青杨 <i@do.codes>
2018-03-17 16:58:32 +0800

    Init

---
+++ src/component/Message/index.js
@@ -0,0 +1,123 @@
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Input from '../Input';
+import CollapsedIcon from '../../media/collapsed.svg';
+import { noop, typesMap } from '../../utils';
+
+import './index.scss';
+
+
+
+class Message extends Component {
+    static propTypes = {
+        value: PropTypes.array.isRequired,
+        name: PropTypes.string,
+        collapsed: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]).isRequired,
+        nestedDepth: PropTypes.number.isRequired,
+        onChange: PropTypes.func
+    };
+    static defaultProps = {
+        onChange: noop
+    };
+
+    constructor(...args) {
+        super(...args);
+        const { nestedDepth, collapsed } = this.props;
+        this.state = {
+            collapsed: typeof collapsed === 'number' ?
+                nestedDepth >= collapsed : !!collapsed
+        };
+    }
+
+    state = { collapsed: false };
+
+    handleToggleCollapsed = (collapsed) => {
+        this.setState({
+            collapsed: typeof collapsed === 'boolean' ?
+                collapsed : !this.state.collapsed
+        });
+    };
+
+    generateOnChange = (node) => {
+        return (e, value) => {
+            const { value: newValue } = this.props;
+            newValue.find(i => i === node).value = value;
+            this.props.onChange(e, newValue);
+        };
+    };
+    renderInput = (node) => {
+        const { name, type, value } = node;
+        return (
+            <div className="tree-input-item" key={name}>
+                <div className="tree-input-item-info">
+                    <span className="tree-input-item-name">"{name}"</span>
+                    <span className="tree-input-item-type">: {type}</span>
+                </div>
+                <Input
+                    name={name}
+                    type={type}
+                    value={value}
+                    onChange={this.generateOnChange(node)}
+                />
+            </div>
+        );
+    };
+    renderMessage = (node) => {
+        const { fieldInfo, name } = node;
+        const { nestedDepth, collapsed } = this.props;
+        return (
+            <Message
+                key={name}
+                value={fieldInfo}
+                name={name}
+                nestedDepth={nestedDepth + 1}
+                collapsed={collapsed}
+                onChange={this.generateOnChange(node)}
+            />
+        );
+    };
+    renderRepeated = (node) => {
+        // TODO: REPEATED
+    };
+    renderNode = (node) => {
+        const { label, type } = node;
+        if (label === 'REPEATED') {
+            return this.renderRepeated(node);
+        } else {
+            return (type === 'message' ? this.renderMessage : this.renderInput)(node);
+        }
+    };
+    render() {
+        const { collapsed } = this.state;
+        const { value, value: { length }, name } = this.props;
+        return (
+            <div
+                className={`tree-input${collapsed ? ' tree-input-collapsed' : ''}`}
+                key={name}
+            >
+                <div
+                    className="tree-input-start"
+                    onClick={this.handleToggleCollapsed}
+                >
+                    <img
+                        className="tree-input-expand"
+                        src={CollapsedIcon}
+                    />
+                    <span className="tree-input-name">"{name}": </span>
+                    <span>{"\u007b"}</span>
+                    <span if={collapsed}> ... }</span>
+                    <span className="tree-input-count">{length} Items</span>
+                </div>
+                <div className="tree-input-items">{value.map(this.renderNode)}</div>
+                <div className="tree-input-end">
+                    <span onClick={this.handleToggleCollapsed}>{"\u007d"}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Message;
--- src/index.js
+++ src/index.js
@@ -2,26 +2,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import Input from './component/Input';
-import CollapsedIcon from './media/collapsed.svg';
+import Message from './component/Message';
 import { noop, typesMap } from './utils';
-
-import './index.scss';
 
 
 
 class TreeInput extends Component {
     static propTypes = {
         schema: PropTypes.array.isRequired,
-        name: PropTypes.string,
+        rootName: PropTypes.string,
         collapsed: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
-        nestedDepth: PropTypes.number,
         onChange: PropTypes.func
     };
     static defaultProps = {
         name: 'Root',
-        onChange: noop,
-        nestedDepth: 0
+        onChange: noop
     };
 
     constructor(...args) {
@@ -49,114 +44,45 @@ class TreeInput extends Component {
     };
     format = (value) => {
         value = value.map(i => {
-            const { name, type, label, value } = i;
-            if (type === 'message') {
-                return `${name}:${this.formatMessage(value)}`;
-            } else if (label === 'REPEATED') {
-                return `${name}:${this.formatRepeated(value)}`;
+            const {
+                name, type, label, value, fieldInfo
+            } = i;
+            if (label === "REPEATED") {
+                return `"${name}":${this.formatRepeated(value)}`;
+            } else if (type === 'message') {
+                return `"${name}":${this.format(fieldInfo)}`;
             } else {
                 if ([undefined, null, ''].includes(value)) return false;
                 if ([typesMap.BYTES, typesMap.STRING].includes(type)) {
-                    return `${name}:"${value}"`;
+                    return `"${name}":"${value}"`;
                 } else {
-                    return `${name}:${value}`;
+                    return `"${name}":${value}`;
                 }
             }
         }).filter(i => !!i).join(',');
         return `{${value}}`;
     };
-    formatMessage = (value) => {
-        console.log(value);
-        return '';
-    };
     formatRepeated = (value) => {
-
+        return `""`;
     };
 
-    generateOnChange = (node) => {
-        return (e, value) => {
-            this.setState((prevState) => {
-                const { value: V } = prevState;
-                V.find(i => i === node).value = value;
-                const newValue = [...V];
-                const formated = this.props.nestedDepth === 0 ? this.format(newValue) : newValue;
-                this.props.onChange(e, formated);
-                return { value: newValue };
-            });
-        };
-    };
-    renderInput = (node) => {
-        const { name, type, value } = node;
-        return (
-            <div className="tree-input-item" key={name}>
-                <div className="tree-input-item-info">
-                    <span className="tree-input-item-name">"{name}"</span>
-                    <span className="tree-input-item-type">: {type}</span>
-                </div>
-                <Input
-                    name={name}
-                    type={type}
-                    value={value}
-                    onChange={this.generateOnChange(node)}
-                />
-            </div>
-        );
-    };
-    renderMessage = (node) => {
-        const { fieldInfo, name } = node;
-        const { nestedDepth, collapsed } = this.props;
-        return (
-            <TreeInput
-                key={name}
-                schema={fieldInfo}
-                name={name}
-                nestedDepth={nestedDepth + 1}
-                collapsed={collapsed}
-                onChange={this.generateOnChange(node)}
-            />
-        );
-    };
-    renderRepeated = (node) => {
-        // TODO: REPEATED
-    };
-    renderNode = (node) => {
-        const { label, type } = node;
-        if (label === 'REPEATED') {
-            return this.renderRepeated(node);
-        } else {
-            return (type === 'message' ? this.renderMessage : this.renderInput)(node);
-        }
+    onChange = (e, value) => {
+        this.setState({ value });
+        const formated = this.format(value);
+        console.log(value, formated);
+        console.log(JSON.parse(formated));
     };
     render() {
-        const { value, value: { length }, collapsed } = this.state;
+        const { value, collapsed } = this.state;
         const { name } = this.props;
         return (
-            <div
-                className={`tree-input${collapsed ? ' tree-input-collapsed' : ''}`}
-                key={name}
-            >
-                <div
-                    className="tree-input-start"
-                    onClick={this.handleToggleCollapsed}
-                >
-                    <img
-                        className="tree-input-expand"
-                        src={CollapsedIcon}
-                        onClick={this.handleToggleCollapsed}
-                    />
-                    <span
-                        className="tree-input-name"
-                        onClick={this.handleToggleCollapsed}
-                    >"{name}": </span>
-                    <span onClick={this.handleToggleCollapsed}>{"\u007b"}</span>
-                    <span if={collapsed} onClick={this.handleToggleCollapsed}> ... }</span>
-                    <span className="tree-input-count">{length} Items</span>
-                </div>
-                <div className="tree-input-items">{value.map(this.renderNode)}</div>
-                <div className="tree-input-end">
-                    <span onClick={this.handleToggleCollapsed}>{"\u007d"}</span>
-                </div>
-            </div>
+            <Message
+                value={value}
+                collapsed={collapsed}
+                nestedDepth={1}
+                name={name}
+                onChange={this.onChange}
+            />
         );
     }
 }
